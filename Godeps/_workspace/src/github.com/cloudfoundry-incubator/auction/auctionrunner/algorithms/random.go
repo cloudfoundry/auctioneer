9cdb817460abf1e79f9a4aa0da393595cad58b9f
pivotal <pivotal@metson.sf.pivotallabs.com>
2014-08-22 11:09:53 -0700

    bump godeps

---
+++ Godeps/_workspace/src/github.com/cloudfoundry-incubator/auction/auctionrunner/algorithms/random.go
@@ -0,0 +1,48 @@
+package algorithms
+
+import (
+	"time"
+
+	"github.com/cloudfoundry-incubator/auction/auctiontypes"
+)
+
+/*
+
+Pick an arbitrary rep
+	Tell it to reserve
+		If the reservation succeeds -- we have a winner
+
+*/
+
+func RandomAuction(client auctiontypes.RepPoolClient, auctionRequest auctiontypes.StartAuctionRequest) (string, int, int, []auctiontypes.AuctionEvent) {
+	rounds, numCommunications := 1, 0
+	auctionInfo := auctiontypes.NewStartAuctionInfoFromLRPStartAuction(auctionRequest.LRPStartAuction)
+	events := []auctiontypes.AuctionEvent{}
+
+	for ; rounds <= auctionRequest.Rules.MaxRounds; rounds++ {
+		t := time.Now()
+		randomPick := auctionRequest.RepGuids.RandomSubsetByCount(1)[0]
+
+		numCommunications += 1
+		reservations := client.RebidThenTentativelyReserve([]string{randomPick}, auctionInfo)
+		events = append(events, auctiontypes.AuctionEvent{"reserve", time.Since(t), rounds, 1, ""})
+
+		if len(reservations) == 0 {
+			events = append(events, auctiontypes.AuctionEvent{"reservation-failed", 0, rounds, 0, "empty"})
+			continue
+		}
+		if reservations[0].Error != "" {
+			events = append(events, auctiontypes.AuctionEvent{"reservation-failed", 0, rounds, 0, reservations[0].Error})
+			continue
+		}
+
+		t = time.Now()
+		numCommunications += 1
+		client.Run(randomPick, auctionRequest.LRPStartAuction)
+		events = append(events, auctiontypes.AuctionEvent{"run", time.Since(t), rounds, 1, ""})
+
+		return randomPick, rounds, numCommunications, events
+	}
+
+	return "", rounds, numCommunications, events
+}
--- Godeps/_workspace/src/github.com/cloudfoundry-incubator/auction/auctionrunner/auction_runner.go
+++ Godeps/_workspace/src/github.com/cloudfoundry-incubator/auction/auctionrunner/auction_runner.go
@@ -4,13 +4,14 @@ import (
 	"errors"
 	"time"
 
+	"github.com/cloudfoundry-incubator/auction/auctionrunner/algorithms"
 	"github.com/cloudfoundry-incubator/auction/auctiontypes"
 )
 
 var AllBiddersFull = errors.New("all the bidders were full")
 
 var DefaultStartAuctionRules = auctiontypes.StartAuctionRules{
-	Algorithm:              "reserve_n_best",
+	Algorithm:              "compare_to_percentile",
 	MaxRounds:              40,
 	MaxBiddingPoolFraction: 0.2,
 	MinBiddingPool:         10,
@@ -28,23 +29,20 @@ func New(client auctiontypes.RepPoolClient) *auctionRunner {
 
 func (a *auctionRunner) RunLRPStartAuction(auctionRequest auctiontypes.StartAuctionRequest) (auctiontypes.StartAuctionResult, error) {
 	result := auctiontypes.StartAuctionResult{
-		LRPStartAuction: auctionRequest.LRPStartAuction,
+		LRPStartAuction:  auctionRequest.LRPStartAuction,
+		AuctionStartTime: time.Now(),
 	}
 
 	t := time.Now()
 	switch auctionRequest.Rules.Algorithm {
 	case "all_rebid":
-		result.Winner, result.NumRounds, result.NumCommunications = allRebidAuction(a.client, auctionRequest)
-	case "all_reserve":
-		result.Winner, result.NumRounds, result.NumCommunications = allReserveAuction(a.client, auctionRequest)
-	case "pick_among_best":
-		result.Winner, result.NumRounds, result.NumCommunications = pickAmongBestAuction(a.client, auctionRequest)
+		result.Winner, result.NumRounds, result.NumCommunications, result.Events = algorithms.AllRebidAuction(a.client, auctionRequest)
+	case "compare_to_percentile":
+		result.Winner, result.NumRounds, result.NumCommunications, result.Events = algorithms.CompareToPercentileAuction(a.client, auctionRequest)
 	case "pick_best":
-		result.Winner, result.NumRounds, result.NumCommunications = pickBestAuction(a.client, auctionRequest)
-	case "reserve_n_best":
-		result.Winner, result.NumRounds, result.NumCommunications = reserveNBestAuction(a.client, auctionRequest)
+		result.Winner, result.NumRounds, result.NumCommunications, result.Events = algorithms.PickBestAuction(a.client, auctionRequest)
 	case "random":
-		result.Winner, result.NumRounds, result.NumCommunications = randomAuction(a.client, auctionRequest)
+		result.Winner, result.NumRounds, result.NumCommunications, result.Events = algorithms.RandomAuction(a.client, auctionRequest)
 	default:
 		panic("unkown algorithm " + auctionRequest.Rules.Algorithm)
 	}
@@ -64,7 +62,7 @@ func (a *auctionRunner) RunLRPStopAuction(auctionRequest auctiontypes.StopAuctio
 
 	var err error
 	t := time.Now()
-	result.Winner, result.NumCommunications, err = stopAuction(a.client, auctionRequest)
+	result.Winner, result.NumCommunications, err = algorithms.StopAuction(a.client, auctionRequest)
 	result.BiddingDuration = time.Since(t)
 
 	return result, err
