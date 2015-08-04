f0326c10edcbe571ad74a10d3aa0cae88fc671d4
Luan Santos <lsantos@pivotal.io>
2015-08-04 14:59:24 -0700

    Use handlers provided by BBS
    [#98039180]
    
    Signed-off-by: Alex Suraci <asuraci@pivotal.io>

--- cmd/auctioneer/main_suite_test.go
+++ cmd/auctioneer/main_suite_test.go
@@ -6,8 +6,8 @@ import (
 	"os/exec"
 	"strings"
 
-	"github.com/cloudfoundry-incubator/auctioneer"
 	"github.com/cloudfoundry-incubator/bbs"
+	"github.com/cloudfoundry-incubator/bbs/auctionhandlers"
 	bbstestrunner "github.com/cloudfoundry-incubator/bbs/cmd/bbs/testrunner"
 	"github.com/cloudfoundry-incubator/consuladapter"
 	"github.com/cloudfoundry-incubator/consuladapter/consulrunner"
@@ -50,7 +50,7 @@ var etcdClient storeadapter.StoreAdapter
 var consulRunner *consulrunner.ClusterRunner
 var consulSession *consuladapter.Session
 
-var auctioneerClient auctioneer.Client
+var auctioneerClient auctionhandlers.Client
 
 var bbsArgs bbstestrunner.Args
 var bbsBinPath string
@@ -99,7 +99,7 @@ var _ = SynchronizedBeforeSuite(func() []byte {
 		"http",
 	)
 
-	auctioneerClient = auctioneer.NewClient(auctioneerAddress)
+	auctioneerClient = auctionhandlers.NewClient(auctioneerAddress)
 
 	logger = lagertest.NewTestLogger("test")
 
