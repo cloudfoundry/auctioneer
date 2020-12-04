8cd9c800fbf99b34c23f219f360fa4fba678c5bd
Amin Jamali <ajamali@pivotal.io>
2020-12-04 18:37:39 +0000

    Add domain SAN for go 1.15 changes
    Addresses cloudfoundry/diego-release#541
    
    [#175789203]

--- cmd/auctioneer/fixtures/regenerate-certs.sh
+++ cmd/auctioneer/fixtures/regenerate-certs.sh
@@ -8,30 +8,30 @@ pushd "$this_dir"
 
 rm -rf out
 certstrap init --common-name "ca" --passphrase ""
-certstrap request-cert --common-name "client" --passphrase "" --ip "127.0.0.1"
+certstrap request-cert --common-name "client" --domain "client" --passphrase "" --ip "127.0.0.1"
 certstrap sign client --CA "ca"
 
-certstrap request-cert --common-name "server" --passphrase "" --ip "127.0.0.1"
+certstrap request-cert --common-name "server" --domain "server" --passphrase "" --ip "127.0.0.1"
 certstrap sign server --CA "ca"
 
 mv -f out/* ./blue-certs/
 rm -rf out
 
 certstrap init --common-name "ca" --passphrase ""
-certstrap request-cert --common-name "client" --passphrase "" --ip "127.0.0.1"
+certstrap request-cert --common-name "client" --domain "client" --passphrase "" --ip "127.0.0.1"
 certstrap sign client --CA "ca"
 
-certstrap request-cert --common-name "server" --passphrase "" --ip "127.0.0.1"
+certstrap request-cert --common-name "server" --domain "server" --passphrase "" --ip "127.0.0.1"
 certstrap sign server --CA "ca"
 
 mv -f out/* ./green-certs/
 rm -rf out
 
 certstrap init --common-name "CA" --passphrase ""
-certstrap request-cert --common-name "client" --passphrase ""
+certstrap request-cert --common-name "client" --domain "client" --passphrase ""
 certstrap sign client --CA "CA"
 
-certstrap request-cert --common-name "metron" --passphrase ""
+certstrap request-cert --common-name "metron" --domain "metron" --passphrase ""
 certstrap sign metron --CA "CA"
 
 mv -f out/* ./metron/
