4c948374cb660bb7116a4a4edd6709a62316db3e
Amin Jamali <71907054+winkingturtle-vmw@users.noreply.github.com>
2023-11-14 15:50:42 +0000

    With oss ci export (#16)
    * use relative path instead of direnv
    
    imported env
    
    * Regenerate expiring certs
    
    Signed-off-by: Amin Jamali <ajamali@vmware.com>
    
    ---------
    
    Signed-off-by: Amin Jamali <ajamali@vmware.com>
    Co-authored-by: Marc Paquette <mpaquette@vmware.com>

--- cmd/auctioneer/main_test.go
+++ cmd/auctioneer/main_test.go
@@ -79,7 +79,7 @@ var _ = Describe("Auctioneer", func() {
 	)
 
 	BeforeEach(func() {
-		fixturesPath := path.Join(os.Getenv("DIEGO_RELEASE_DIR"), "src/code.cloudfoundry.org/auctioneer/cmd/auctioneer/fixtures")
+		fixturesPath := "fixtures"
 
 		caFile := path.Join(fixturesPath, "green-certs", "ca.crt")
 		clientCertFile := path.Join(fixturesPath, "green-certs", "client.crt")
